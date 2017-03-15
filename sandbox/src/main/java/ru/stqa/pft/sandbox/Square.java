74decd22964db3c890e6b1c3e0a7eb77d4ba94d8
Valeria Bozhkova <bozhkova.valeria@gmail.com>
2017-03-15 15:54:24 +0300

    Добавила новые классы для квадрата и прямоугольника

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by user on 3/14/2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l*this.l;
+  }
+
+}
+
