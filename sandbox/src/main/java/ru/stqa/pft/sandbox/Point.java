14d2c08637ab8ceb260ac8b3e72127e36cf35c60
Valeria Bozhkova <bozhkova.valeria@gmail.com>
2017-03-23 07:52:02 +0300

    Убрала округление в методе distance в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -15,6 +15,6 @@ public class Point {
 
   public double distance(Point p2) {
 
-    return Math.round(Math.sqrt(Math.pow((p2.x - this.x),2) + Math.pow((p2.y - this.y),2)));
+    return Math.sqrt(Math.pow((p2.x - this.x),2) + Math.pow((p2.y - this.y),2));
   }
 }
