00783d2ebeaa7adb2129adcb3a75aa0add8fa076
Valeria Bozhkova <bozhkova.valeria@gmail.com>
2017-03-31 09:32:39 +0300

    Добавила условия для вырожденного уравнения

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -18,10 +18,21 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (d > 0) {
-      n = 2;
+    if (a == 0) {
+      if (b == 0) {
+        if (c == 0) {
+          n = -1;
+        } else {
+          n = 0;
+        }
+      } else {
+        n = 1;
+      }
+
     } else {
-      if (d == 0) {
+      if (d > 0) {
+        n = 2;
+      } else if (d == 0) {
         n = 1;
       } else {
         n = 0;
