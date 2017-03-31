27d873d8c3d5829acc5f08ef6951480e09966ec9
Valeria Bozhkova <bozhkova.valeria@gmail.com>
2017-03-31 09:40:47 +0300

    Переделала условие if-else на более читаемое

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Equation.java
@@ -18,18 +18,7 @@ public class Equation {
 
     double d = b * b - 4 * a * c;
 
-    if (a == 0) {
-      if (b == 0) {
-        if (c == 0) {
-          n = -1;
-        } else {
-          n = 0;
-        }
-      } else {
-        n = 1;
-      }
-
-    } else {
+    if (a != 0) {
       if (d > 0) {
         n = 2;
       } else if (d == 0) {
@@ -37,7 +26,17 @@ public class Equation {
       } else {
         n = 0;
       }
+
+    } else if (b != 0) {
+      n = 1;
+      
+    } else if (c != 0) {
+      n = 0;
+
+    } else {
+      n = -1;
     }
+
   }
 
   public int rootNumber() {
