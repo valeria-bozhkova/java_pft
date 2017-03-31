7f4844e2452a2d56f10dc78365e1909e60200baa
Valeria Bozhkova <bozhkova.valeria@gmail.com>
2017-03-31 15:26:56 +0300

    Оптимизировала переход для gotoAddContactPage

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -23,6 +23,10 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoAddContactPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+      return;
+    }
     click(By.linkText("add new"));
   }
 
